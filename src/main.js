1e8dee77bf260523714de79e16105d069dbcca86
Fernando Arellano <farellano0@ucol.mx>
2020-12-09 15:54:31 -0600

    implemented factorial()

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
-export default class App {}
+export default class App {
+
+    factorial(numero){
+        let fact = 1;
+
+        for(let i = numero; i > 0; i = i - 1){
+            fact = i * fact;
+        }
+        return fact;
+    }
+}
 
 let app = new App();
+
+console.log(app.factorial(5));
